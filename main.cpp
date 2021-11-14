1d6d5222fa0e9534a1e11e385985b007c03d8236
manuelafedi <91663595+manuelafedi@users.noreply.github.com>
2021-11-14 17:00:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
+   int n=0,b=1,a=0,c=0;
+   cin>>n;
+   
+   cout<<b<<endl;
+   for(c=0;c<n;c++)
+   {
+      c=a+b;
+      cout<<c<<endl;
+      a=b;
+      b=c;
+   }
+   
    return 0;
 }
